4a620562196ec4340a617e3e8db80392fa810516
bradlm <bradmelluish@gmail.com>
2016-08-10 19:00:28 -0700

    Implements Queue
    Implements queue backend. Does not yet have view.

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -6,7 +6,7 @@ var AppModel = Backbone.Model.extend({
     this.set('songQueue', new SongQueue());
 
     /* Note that 'this' is passed as the third argument. That third argument is
-    the context. The 'play' handler will always be bound to that context we pass in.
+    the context. The 'play' handler will always be bound to that context we pass in.-
     In this example, we're binding it to the App. This is helpful because otherwise
     the 'this' we use that's actually in the function (this.set('currentSong', song)) would
     end up referring to the window. That's just what happens with all JS events. The handlers end up
@@ -14,8 +14,16 @@ var AppModel = Backbone.Model.extend({
 
 
     params.library.on('play', function(song) {
-      this.set('currentSong', song);
+      if (!this.get('currentSong').get('url')) {
+        this.set('currentSong', song);
+      }
+      this.get('songQueue').push(song);
+      //console.log(this.get('songQueue').at(this.get('songQueue').length - 1).get('title'))
     }, this);
+  },
+  nextSong: function() {
+    this.get('songQueue').shift();
+    this.set('currentSong', this.get('songQueue').at(0));
   }
-
+  
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,9 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+  enqueue: function() { //may want to break out into seperate function from play, if we want play to change current song while preserving the remaining queue.
+    this.trigger('play', this);
   }
 
 });
