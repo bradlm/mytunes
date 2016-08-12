3c523eff83cabad5ac826acf2e80c62491579226
bradlm <bradmelluish@gmail.com>
2016-08-11 19:02:55 -0700

    Polish
    Polish.

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -1,4 +1,6 @@
 // Put your parse application keys here!
+// Application-Id: OWawBR069jvFwWln4zrgOJR2wOCN6oetbBdvMLJq
+// REST-API-Key: GH78f2KLgWQT16v2OtQjI0WJZXBd7czwegMoIogB
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
